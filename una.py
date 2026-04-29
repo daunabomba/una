6ac00dd51a6b3e313d50fdb9b9287634eb8fb8aa
daunabomba <daunabomba@github.com>
2026-04-29 08:10:08 +0800

    una

--- una.py
+++ una.py
@@ -758,8 +758,8 @@ def main():
         colors.info("Starting build process.")
         all_possible_arches = ["x32", "x86_64", "aarch64", "riscv64"]
         
-        tools_marker = tools_install_dir / ".tools_built"
-        tools_state_file = tools_install_dir / ".tools_state"
+        tools_marker = tools_install_dir / "tools_built"
+        tools_state_file = tools_install_dir / "tools_state"
 
         import subprocess
 
